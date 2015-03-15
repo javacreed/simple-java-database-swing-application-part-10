da0db67787e33001bca9a06216d5dac3e4aec339
javacreed <albert@javacreed.com>
2015-03-15 11:35:30 +0100

    Initial Import

---
+++ src/main/java/com/javacreed/examples/app/Main.java
@@ -0,0 +1,58 @@
+/*
+ * #%L
+ * Simple Java Database Swing Application - Part 10 - Simple User Interface
+ * %%
+ * Copyright (C) 2012 - 2015 Java Creed
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.javacreed.examples.app;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+  public static void main(final String[] args) {
+    DbHelper.getInstance().init();
+    DbHelper.getInstance().registerShutdownHook();
+
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        Main.LOGGER.debug("Starting application");
+
+        final Application app = new Application();
+        app.setTitle("Simple Java Database Swing Application");
+        app.setSize(800, 600);
+        app.setLocationRelativeTo(null);
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setVisible(true);
+
+        // app.addWindowListener(new WindowAdapter() {
+        // @Override
+        // public void windowClosing(WindowEvent e) {
+        // Main.LOGGER.info("Done");
+        // DbHelper.getInstance().close();
+        // }
+        // });
+      }
+    });
+  }
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+}
